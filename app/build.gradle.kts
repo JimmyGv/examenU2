831e951a2bfc37cf6b2d5136ff99061c1fa82803
JimmyGv <tmp_jgonzalez@accitesz.com>
2023-11-08 11:31:58 -0600

    Update two

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.examenunidad2"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.examenunidad2"
